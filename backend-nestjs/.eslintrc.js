23a14325a377bf9ab91e8646d4fbf2cb39a4cee7
Reza Mirzapour <mirzaandrampage@gmail.com>
2021-08-20 03:10:44 +0430

    fix some wrong spell

--- backend-nestjs/.eslintrc.js
+++ backend-nestjs/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'backend/tsconfig.json',
+    project: 'backend-nestjs/tsconfig.json',
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
