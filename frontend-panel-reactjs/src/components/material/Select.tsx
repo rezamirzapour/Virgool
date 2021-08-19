12c3ad29be4d305eacbdd12e55db1429ec6c6b1f
rezamirzapour <mirzaandrampage@gmail.com>
2021-08-19 20:32:11 +0330

    compatible Select component with react-hook-form

--- frontend-panel-reactjs/src/components/material/Select.tsx
+++ frontend-panel-reactjs/src/components/material/Select.tsx
@@ -1,25 +1,29 @@
-import { Select as MaterialSelect, MenuItem, makeStyles, SelectProps } from '@material-ui/core';
+import { Select as MaterialSelect, MenuItem, SelectProps } from '@material-ui/core';
+import { Controller, Control } from 'react-hook-form'
 
 interface ISelectProps extends SelectProps {
     options: [{
         label: string,
         value: any
-    }]
-}
-export function Select(props: ISelectProps) {
-    const classes = useStyles();
-    const { options, ...otherProps } = props
-    return <MaterialSelect
-        className={classes.root}
-        variant={props.variant ?? "outlined"}
-        fullWidth={props.fullWidth ?? true}
-        {...otherProps}>
-        {options.map(o => <MenuItem value={o.value}>{o.label}</MenuItem>)}
-    </MaterialSelect>
-}
-
-const useStyles = makeStyles({
-    root: {
-
+    }],
+    methods: {
+        control: Control,
+        errors: any
     }
-});
+}
+export function Select({ methods, options, ...rest }: ISelectProps) {
+    return <Controller
+        name={rest.name as string}
+        control={methods.control}
+        defaultValue={rest.defaultValue ?? ""}
+        render={(otherProps) => <MaterialSelect
+            variant={rest.variant ?? "outlined"}
+            fullWidth={rest.fullWidth ?? true}
+            {...rest}
+            {...otherProps}
+        >
+            {options.map(o => <MenuItem value={o.value}>{o.label}</MenuItem>)}
+        </MaterialSelect>
+        }
+    />
+}
